73db20097344998384fb3f24b298710d368294f4
JosefStonon <josef.sartori@outlook.com>
2023-01-28 02:00:58 -0300

    react-native

---
+++ app/src/Main/index.tsx
@@ -0,0 +1,60 @@
+
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  Footer,
+  FooterContainer }
+  from './styles';
+
+import { Header } from '../components/Header/index';
+import { Categories } from '../components/Categories';
+import { Menu } from '../components/Menu';
+import { Button } from '../components/Button';
+import { TableModal } from '../components/TableModal';
+import { useState } from 'react';
+
+export function Main() {
+  const [isTableModalVisible, setTableModalVisible] = useState(false);
+  const [selectedTable, setSelectTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectTable(table);
+  }
+
+  return (
+
+    <>
+      <Container>
+
+        <Header />
+
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+
+      </Container>
+
+      <Footer>
+        <FooterContainer>
+          {!selectedTable && (
+            <Button onPress={() => setTableModalVisible(true)} >
+            Novo Pedido
+            </Button>
+          )}
+        </FooterContainer>
+      </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
+
+    </>
+  );
+}
---
+++ app/src/mocks/products.ts
@@ -0,0 +1,126 @@
+export const products = [
+
+  {
+          "_id": "63c336841fcbdc6bca0189aa",
+					"name": "Chicken-Duplo",
+					"description": "Grelhado",
+					"imagePath": "1673737860522-chicken.png",
+					"price": 80,
+					"ingredients": [
+						{
+							"name": "laranja",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ab"
+						},
+						{
+							"name": "Laranja",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ac"
+						},
+						{
+							"name": "Gouda",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ad"
+						},
+						{
+							"name": "Brie",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ae"
+						}
+					],
+					"category": "63c336591fcbdc6bca0189a7",
+					"__v": 0
+				},
+        {
+          "_id": "63c337341fcbdc6bca0189c4",
+					"name": "Suco de Laranja",
+					"description": "Natural",
+					"imagePath": "1673738036397-suco-de-laranja.png",
+					"price": 15,
+					"ingredients": [
+						{
+							"name": "laranja",
+							"icon": "🧀",
+							"_id": "63c337341fcbdc6bca0189c5"
+						},
+						{
+							"name": "Laranja",
+							"icon": "🧀",
+							"_id": "63c337341fcbdc6bca0189c6"
+						},
+						{
+							"name": "Gouda",
+							"icon": "🧀",
+							"_id": "63c337341fcbdc6bca0189c7"
+						},
+						{
+							"name": "Brie",
+							"icon": "🧀",
+							"_id": "63c337341fcbdc6bca0189c8"
+						}
+          ]
+        },
+
+
+			{
+
+					"_id": "63c336841fcbdc6bca0189ab",
+					"name": "X-Frango",
+					"description": "Grelhado",
+					"imagePath": "1673737860522-chicken.png",
+					"price": 28,
+					"ingredients": [
+						{
+							"name": "laranja",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ab"
+						},
+						{
+							"name": "Laranja",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ac"
+						},
+						{
+							"name": "Gouda",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ad"
+						},
+						{
+							"name": "Brie",
+							"icon": "🧀",
+							"_id": "63c336841fcbdc6bca0189ae"
+						}
+					]
+				},
+
+        {
+          "_id": "63c336d91fcbdc6bca0189b5",
+					"name": "X-Burguer",
+					"description": "Duplo",
+					"imagePath": "1673737945681-burger-molho-especial.png",
+					"price": 30,
+					"ingredients": [
+						{
+							"name": "laranja",
+							"icon": "🧀",
+							"_id": "63c336d91fcbdc6bca0189b6"
+						},
+						{
+							"name": "Laranja",
+							"icon": "🧀",
+							"_id": "63c336d91fcbdc6bca0189b7"
+						},
+						{
+							"name": "Gouda",
+							"icon": "🧀",
+							"_id": "63c336d91fcbdc6bca0189b8"
+						},
+						{
+							"name": "Brie",
+							"icon": "🧀",
+							"_id": "63c336d91fcbdc6bca0189b9"
+						}
+					],
+        }
+
+]
