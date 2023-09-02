10a194aab895aeebdd8bcc3947a971b257e482dd
JosefStonon <josef.sartori@outlook.com>
2023-09-02 00:34:03 -0300

    feat: cors config

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,6 +9,14 @@ mongoose.connect('mongodb://127.0.0.1:27017')
     const app = express();
     const port = 3001;
 
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+
+    });
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
